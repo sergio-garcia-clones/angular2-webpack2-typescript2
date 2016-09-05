97c9f19e4a6646ea91796e92f5457a5f2699088f
Sasha Vincic <sasha@vincic.org>
2016-09-05 23:01:42 +0200

    make the height big enough to show the whole menu

--- src/frontend/app/start.component.ts
+++ src/frontend/app/start.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'start',
+    styles: ['h1 { margin-bottom: 300px; }'],
     template: '<h1>Start</h1>'
 })
 export class StartComponent implements OnInit {
